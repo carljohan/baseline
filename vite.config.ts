8ef765093ba66270398ffbedcb08f652ed3176a4
carljohan <carljohan@hey.com>
2024-06-18 22:07:04 -0400

    build: specify vite base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-
+  base: "/baseline/",
   resolve: {
     alias: {
       $lib: path.resolve("./src/lib"),
